e3af7d4fadab4ef97b4cadaa0b794b3437ae00c0
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-11 14:46:33 +0900

    자바 프로그램 실행 - 텍스트 출력

---
+++ src/HelloJava.java
@@ -0,0 +1,5 @@
+public class HelloJava { // HelloJava class 범위 시작
+    public static void main(String[] args) { // main() 메서드 범위 시작
+        System.out.println("hello java"); // hello java 출력
+    } // main() 메서드 범위 끝
+} // HelloJava class 범위 끝
