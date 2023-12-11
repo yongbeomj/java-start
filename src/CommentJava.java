2b9630be0733d35e6ba96cdda8047171680b9c0a
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-11 14:50:19 +0900

    주석(comment)

---
+++ src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1 을 출력
+        // System.out.println("hello java2"); 한 줄 주석
+
+        /* 여러 줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java3");
+        */
+    }
+}
