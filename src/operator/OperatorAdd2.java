a8d8a03b4bf6159744fbc9895bc4dd5e955c1fba
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-12 14:26:11 +0900

    증감 연산자

---
+++ src/operator/OperatorAdd2.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class OperatorAdd2 {
+    public static void main(String[] args) {
+        // 전위 증감 연산자
+        int a = 1;
+        int b = 0;
+
+        b = ++a; // a값을 먼저 증가시키고, 그 값을 b에 대입
+//        a = a + 1;
+//        b = a;
+        System.out.println("a = " + a + ", b = " + b);
+
+        // 후위 증감 연산자
+        a = 1;
+        b = 0;
+
+        b = a++; // a값을 b에 대입하고, 그 후 a값을 증가시킴
+//        b = a;
+//        a = a + 1;
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
