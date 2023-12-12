737bcc8ff2dc0cba5b0cdf462c6ca6d5db02dcc4
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-12 11:05:34 +0900

    변수 타입

---
+++ src/variable/Var7.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // true, false
+        char d = 'A'; // 문자 하나, 작은 따옴표 사용
+        String e = "Hello Java"; // 문자열, 큰 따옴표 사용
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        // 리터럴 : 개발자가 실제 입력한 값 (하드코딩 한 값)
+    }
+}
