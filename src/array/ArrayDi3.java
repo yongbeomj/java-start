4b05bebc53796837b1742546f9260e9b2c1fa59f
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-13 16:54:38 +0900

    2차원 배열

---
+++ src/array/ArrayDi3.java
@@ -0,0 +1,19 @@
+package array;
+
+public class ArrayDi3 {
+    public static void main(String[] args) {
+        // 2x3 2차원 배열을 만든다.
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        // 2차원 배열의 길이를 활용
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
