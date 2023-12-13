daf91beb13123ddde86194234fe956be2905a536
Yongbeom Jang <alex.ybjang@gmail.com>
2023-12-13 13:33:38 +0900

    scanner - 예제 문제

---
+++ src/scanner/ex/ScannerEx9.java
@@ -0,0 +1,28 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerEx9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요:");
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            int num = scanner.nextInt();
+
+            if (num == -1) {
+                break;
+            }
+
+            sum += num;
+            count++;
+        }
+
+        double average = (double) sum / count;
+        System.out.println("입력한 숫자들의 합계: " + sum);
+        System.out.println("입력한 숫자들의 평균: " + average);
+    }
+}
